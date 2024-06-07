4c3b75e5f5b7d87c51317dc157a1b913174e8b7f
Namit15 <92371924+Namit15@users.noreply.github.com>
2024-06-07 20:11:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculate() {
+    var amount = parseFloat(document.getElementById("amount").value);
+    var r = 88.2 / 100;
+    var t = 4.3452380952380;
+
+    var a = amount * Math.pow((1 + r), t);
+    var profit = a - amount;
+    var percentageIncrease = (profit / amount) * 100;
+
+    document.getElementById("calculated-amount").innerText = "The calculated amount is: " + a.toFixed(2);
+    document.getElementById("profit").innerText = "The profit gained is: " + profit.toFixed(2);
+    document.getElementById("percentage-increase").innerText = "The percentage increase is: " + percentageIncrease.toFixed(2) + "%";
+    document.getElementById("result").style.display = "block";
+}
